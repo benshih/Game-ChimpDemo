0acbe137f64b71dc0053f3f10fe0321caf27ba98
Benjamin Shih <benjshih@gmail.com>
2013-08-15 20:05:00 -0400

    fixed up display screen. graphics and sound files are missing so does not work properly, but i have stopped because i feel i understand the general idea of pygame

--- chimp.py
+++ chimp.py
@@ -30,7 +30,7 @@ def load_sound(name):
     try:
         sound = pygame.mixer.Sound(fullname)
     except pygame.error as message:
-        print('Cannot load sound: ', wav)
+        print('Cannot load sound: ', fullname)
         raise SystemExit(message)
     return sound
 
@@ -107,17 +107,70 @@ class Chimp(pygame.sprite.Sprite):
         if not self.dizzy:
             self.dizzy = 1
             self.original = self.image
+
+def main():
     
-# Initialize program components (the reason why nothing was showing up!)
-pygame.init()
-screen = pygame.display.set_mode((468, 60))
-pygame.display.set_caption('monkey fever')
-pygame.mouse.set_visible(0)
+    # Initialize program components (the reason why nothing was showing up!)
+    pygame.init()
+    screen = pygame.display.set_mode((468, 60))
+    pygame.display.set_caption('monkey fever')
+    pygame.mouse.set_visible(0)
 
-# Create the background for the message text.
-background = pygame.Surface(screen.get_size())
-background = background.convert() # makes sure the background is the same format as the display window, which provides the fastest results
-background.fill((250, 250, 250))
+    # Create the background for the message text.
+    background = pygame.Surface(screen.get_size())
+    background = background.convert() # makes sure the background is the same format as the display window, which provides the fastest results
+    background.fill((250, 250, 250))
 
+    # Render centered text on the background.
+    if pygame.font:
+        font = pygame.font.Font(None, 36) # use the default font, size 36
+        text = font.render("pummel the chimp and win $$$", 1, (10, 10, 10))
+        textpos = text.get_rect(centerx=background.get_width()/2)
+        background.blit(text, textpos) # paste the text onto the background image
 
+    # display the background while initializing other resources
+    screen.blit(background, (0,0))
+    pygame.display.flip() # handles single buffered and double buffered surfaces so that they are visible to the user.
 
+    # prepare all the game objects that the game will use.
+    ## sound effects
+    whiff_sound = load_sound('')
+    punch_sound = load_sound('')
+    ## create an instance of each sprite class
+    chimp = Chimp()
+    fist = Fist()
+    ## RenderPlain for simple drawings
+    ## allsprites list contains all the sprites that belong in the group
+    allsprites = pygame.sprite.RenderPlain((fist, chimp))
+    ## control thede game's framerate by making sure the main loop doesn't run too quickly
+    clock = pygame.time.Clock()
+
+    # main game loop
+    while 1:
+        ## makes sure the game doesn't run faster than 60 fps
+        clock.tick(60)
+
+        ## handle input events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                return
+            elif event.type == MOUSEBUTTONDOWN:
+                if fist.punch(chimp):
+                    punch_sound.play()
+                    chimp.punched()
+                else:
+                     whiff_sound.play()
+            elif event.type == MOUSEBUTTONUP:
+                fist.unpunch()
+
+        allsprites.update()
+        
+        ## draw the entire scene
+        screen.blit(background, (0,0))
+        allsprites.draw(screen)
+        pygame.display.flip()
+
+# calls the main function when the python script is executed
+if __name__ == '__main__': main()
